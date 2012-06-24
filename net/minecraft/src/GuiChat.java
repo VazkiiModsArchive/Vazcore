f1daa5de2203a0e52c655c1db3a2ef655666d599
Vazkii <Vazkii@hotmail.com>
2012-06-24 14:58:10 +0100

    Updated sources for 1.1. (New Features, new Commands and bugfixes).

--- net/minecraft/src/GuiChat.java
+++ net/minecraft/src/GuiChat.java
@@ -1,5 +1,6 @@
 package net.minecraft.src;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
@@ -10,6 +11,10 @@ import org.lwjgl.input.Mouse;
 
 public class GuiChat extends GuiScreen
 {
+	boolean hasAddGuiOpen = false;
+	GuiTextField macroContentsField;
+	GuiTextField macroNameField;
+	
     private String field_50062_b = "";
 
     /**
@@ -55,6 +60,28 @@ public class GuiChat extends GuiScreen
         this.inputField.setText(this.defaultInputFieldText);
         this.inputField.setCanLoseFocus(false);
     }
+    
+    void resetFields(){
+    	hasAddGuiOpen = true;
+        this.macroContentsField = new GuiTextField(this.fontRenderer, width - 330, 5, 320, 16);
+        this.macroContentsField.setMaxStringLength(256);
+        this.macroNameField = new GuiTextField(this.fontRenderer, width - 416, 5, 65, 16);
+        this.macroNameField.setMaxStringLength(16);
+    	controlList.add(new GuiButton(0, width-100, 26, 18, 20, "+"));
+    	controlList.add(new GuiButton(1, width-80, 26, 18, 20, "¿"));
+    	controlList.add(new GuiButton(2, width-60, 26, 18, 20, "¶"));
+    	controlList.add(new GuiButton(3, width-40, 26, 18, 20, "X"));
+        this.inputField.setCanLoseFocus(true);
+    }
+    
+    void clearFields(){
+    	hasAddGuiOpen = false;
+    	macroContentsField = null;
+    	macroNameField = null;
+        this.inputField.setFocused(true);
+        this.inputField.setCanLoseFocus(false);
+    	controlList.clear();
+    }
 
     /**
      * Called when the screen is unloaded. Used to disable keyboard repeat events
@@ -71,6 +98,14 @@ public class GuiChat extends GuiScreen
     public void updateScreen()
     {
         this.inputField.updateCursorCounter();
+        if(hasAddGuiOpen){
+        	if(!controlList.isEmpty()){
+            	GuiButton b = (GuiButton) controlList.get(0);
+            	b.enabled = mod_ChatMacros.isMacroValid(macroNameField.getText(), macroContentsField.getText());
+        	}
+            this.macroContentsField.updateCursorCounter();
+            this.macroNameField.updateCursorCounter();
+        }
     }
 
     /**
@@ -113,11 +148,31 @@ public class GuiChat extends GuiScreen
 
             if (var3.length() > 0 && !this.mc.lineIsCommand(var3))
             {
-            	//TODO Not really a todo, but to mark where the processing is. :3
+            	if(hasAddGuiOpen && (macroNameField.getIsFocused() || macroContentsField.getIsFocused())) return;
+            	
             	if(mod_ChatMacros.macrosEnabled){
             	String msg = mod_ChatMacros.messWithMessage(var3);
-            	if(msg != null)
-                mc.thePlayer.sendChatMessage(mod_ChatMacros.messWithMessage(var3));
+            	if(msg != null){
+    				if(msg.contains("¶")){
+    					String[] macroTokens = msg.split("¶");
+    					boolean invalid = false;
+    					for(String s : macroTokens)
+    						if(s.length() == 0 || s.length() > 100) invalid = true;
+    					if(!invalid){
+    						for(String s : macroTokens)
+    							mc.thePlayer.sendChatMessage(s);
+    					            if(mod_ChatMacros.closeOnFinish)
+    					                mc.displayGuiScreen((GuiScreen)null);
+    					                else inputField.setText("");
+    					            return;
+    					}else mc.thePlayer.addChatMessage("§cInvalid Message!");
+    				}else if(msg.equals("╩ÙÜ7Y‼54$Ê4ÝCTE○8▒◘")){ //This code is the code for the add command, it's defined in mod_ChatMacros.
+    					resetFields();
+    					inputField.setText("");
+			            return;
+    				}
+    				mc.thePlayer.sendChatMessage(msg);
+            	}
             	else if(mod_ChatMacros.closeOnFinish)
                     mc.displayGuiScreen((GuiScreen)null);
                 	else inputField.setText("");
@@ -126,7 +181,6 @@ public class GuiChat extends GuiScreen
             if(mod_ChatMacros.closeOnFinish)
             mc.displayGuiScreen((GuiScreen)null);
             else inputField.setText("");
-
         }
         else if (par2 == 200)
         {
@@ -146,7 +200,13 @@ public class GuiChat extends GuiScreen
         }
         else
         {
-            this.inputField.textboxKeyTyped(par1, par2);
+        	if(hasAddGuiOpen){
+            	if(inputField.getIsFocused())
+                    inputField.textboxKeyTyped(par1, par2);
+                	else if(macroContentsField.getIsFocused())
+                	macroContentsField.textboxKeyTyped(par1, par2);
+                	else macroNameField.textboxKeyTyped(par1, par2);
+        	}else inputField.textboxKeyTyped(par1, par2);
         }
     }
 
@@ -201,8 +261,46 @@ public class GuiChat extends GuiScreen
             }
         }
 
+        if(hasAddGuiOpen){
+            macroContentsField.mouseClicked(par1, par2, par3);
+            macroNameField.mouseClicked(par1, par2, par3);
+        }
+
         this.inputField.mouseClicked(par1, par2, par3);
         super.mouseClicked(par1, par2, par3);
+    }
+    
+    protected void actionPerformed(GuiButton par1GuiButton)
+    {
+    	switch(par1GuiButton.id){	
+
+    	case 0:{
+    		if(mod_ChatMacros.addMacroRemotely(macroNameField.getText(), macroContentsField.getText())){
+    			macroContentsField.setText("");
+    			macroNameField.setText("");
+    			try {
+					mod_ChatMacros.readMacros(new File(ModLoader.getMinecraftInstance().getAppDir("minecraft"), "Chat Macros.txt"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+    		}
+    		break;
+    		}	
+    	case 1: {
+    		macroContentsField.setText(macroContentsField.getText().concat("¿"));
+    		macroContentsField.setFocused(true);
+    		break;
+    		}	
+    	case 2: {
+    		macroContentsField.setText(macroContentsField.getText().concat("¶"));
+    		macroContentsField.setFocused(true);
+    		break;
+    		}
+		case 3: {
+			clearFields();
+			break;
+			}
+    	}
     }
 
     public void confirmClicked(boolean par1, int par2)
@@ -344,14 +442,25 @@ public class GuiChat extends GuiScreen
     public void drawScreen(int par1, int par2, float par3)
     {
         drawRect(2, this.height - 14, this.width - 2, this.height - 2, Integer.MIN_VALUE);
-        drawRect(2, this.height - 37, fontRenderer.getStringWidth("Finish Mode: Close Chat Pane.") + 4, this.height - 16, Integer.MIN_VALUE);
+        drawRect(2, this.height - 37, fontRenderer.getStringWidth("Finish Mode: Close Chat Pane. (RCONTROL)") + 6, this.height - 16, Integer.MIN_VALUE);
         this.inputField.drawTextBox();
         if(mod_ChatMacros.macrosEnabled)
-        drawString(fontRenderer, "�9Chat Macros �aEnabled." , 2, height-36, 0xFFFFFF);
-        else drawString(fontRenderer, "�9Chat Macros �cDisabled." , 2, height-36, 0xFFFFFF);
+        drawString(fontRenderer, "§9Chat Macros §aEnabled. §9(ALT)" , 2, height-36, 0xFFFFFF);
+        else drawString(fontRenderer, "§9Chat Macros §cDisabled. §9(ALT)" , 2, height-36, 0xFFFFFF);
         if(mod_ChatMacros.closeOnFinish)
-        drawString(fontRenderer, "�9Finish Mode: Close Chat Pane." , 2, height-26, 0xFFFFFF);
-        else drawString(fontRenderer, "�9Finish Mode: Clear Message." , 2, height-26, 0xFFFFFF);
+        drawString(fontRenderer, "§9Finish Mode: Close Chat Pane. (RCONTROL)" , 2, height-26, 0xFFFFFF);
+        else drawString(fontRenderer, "§9Finish Mode: Clear Message. (RCONTROL)" , 2, height-26, 0xFFFFFF);
+        if(hasAddGuiOpen){
+            macroContentsField.drawTextBox();
+            macroNameField.drawTextBox();
+            drawString(fontRenderer, "»", width-344, 10, 0xFFFFFF);
+            if(mod_ChatMacros.macrosList.contains(macroNameField.getText()) || macroNameField.getText().equalsIgnoreCase("macros") || macroNameField.getText().equalsIgnoreCase("help") || macroNameField.getText().equalsIgnoreCase("reload") || macroNameField.getText().equalsIgnoreCase("add"))
+            drawString(fontRenderer, "That macro already exists.", width - 415, 25, 0xFF0000);
+            else if(macroNameField.getText().isEmpty())
+            drawString(fontRenderer, "You must specify a macro name.", width - 415, 25, 0xFF0000);
+            else if(macroContentsField.getText().isEmpty())
+            drawString(fontRenderer, "You must specify the macro's contents.", width - 415, 25, 0xFF0000);     
+        }
         super.drawScreen(par1, par2, par3);
     }
 }
--- net/minecraft/src/mod_ChatMacros.java
+++ net/minecraft/src/mod_ChatMacros.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -23,6 +24,7 @@ import java.util.TimeZone;
 import net.minecraft.src.vazkii.updatemanager.IUMAdvanced;
 import net.minecraft.src.vazkii.updatemanager.IUpdateManager;
 import net.minecraft.src.vazkii.updatemanager.ModType;
+import net.minecraft.src.vazkii.updatemanager.UMCore;
 
 public class mod_ChatMacros extends BaseMod implements IUpdateManager, IUMAdvanced {
 
@@ -36,10 +38,11 @@ public class mod_ChatMacros extends BaseMod implements IUpdateManager, IUMAdvanc
 	public static boolean closeOnFinish = true;
 	
 	public String getVersion() {
-		return "by Vazkii. Version [1.0] for 1.2.5";
+		return "by Vazkii. Version [1.1] for 1.2.5";
 	}
 
 	public void load() {
+		UMCore.addMod(this);
 		File f = new File(ModLoader.getMinecraftInstance().getAppDir("minecraft"), "Chat Macros.txt");
 			try {
 				if(!f.exists()){
@@ -67,11 +70,11 @@ public class mod_ChatMacros extends BaseMod implements IUpdateManager, IUMAdvanc
         }
         
         for(String macro : macroList){
-        	String [] tokens = macro.split("»");
+        	String [] tokens = macro.split("Â»");
         	if(tokens.length == 2){
         		tokens[0] = tokens[0].replaceAll("	", "");
         		tokens[1] = tokens[1].replaceAll("	", "");
-        		if(tokens[0].equalsIgnoreCase("macros") || tokens[0].equalsIgnoreCase("reload")) continue;
+        		if(tokens[0].equalsIgnoreCase("macros") || tokens[0].equalsIgnoreCase("reload") || tokens[0].equalsIgnoreCase("help") || tokens[0].equalsIgnoreCase("add")) continue;
         		
         		if(tokens[0].equalsIgnoreCase("macrocharstart")){
         			macroCharStart = tokens[1].charAt(0);
@@ -82,6 +85,7 @@ public class mod_ChatMacros extends BaseMod implements IUpdateManager, IUMAdvanc
             		continue;
             	}
             		macros.put(tokens[0], tokens[1]);
+            		if(!macrosList.contains(tokens[0]))
             		macrosList.add(tokens[0]);
             		System.out.println("Put Macro: '" + tokens[0] + "' into '" + tokens[1] + "' at index " + macroCount + ".");
             		macroCount++;
@@ -102,61 +106,75 @@ public class mod_ChatMacros extends BaseMod implements IUpdateManager, IUMAdvanc
 		writer.write("# an example macro you can take reference to.\r");
 		writer.write("#\r");
 		writer.write("# This is the character to begin macros with:\r");
-		writer.write("	macroCharStart	»	{\r");
+		writer.write("	macroCharStart	Â»	{\r");
 		writer.write("# This is the character to finish macros with:\r");
-		writer.write("	macroCharEnd	»	}\r");
+		writer.write("	macroCharEnd	Â»	}\r");
 		writer.write("# Here is an example macro to help you get started, you can copy it and make more:\r");
-		writer.write("# Note that the character '»' is splitting the macro name from the macro itself.\r");
-		writer.write("	modpage	»	http://bit.ly/s6rL8h\r");
+		writer.write("# Note that the character 'Â»' is splitting the macro name from the macro itself.\r");
+		writer.write("	modpage	Â»	http://bit.ly/s6rL8h\r");
 		writer.write("# Or you could add in a code to add variables to the macros:\r");
-		writer.write("# name	»	¿name\r");
+		writer.write("# name	Â»	Â¿name\r");
 		writer.write("# This would type in your username to the chat, but there are more variable codes, here's a list of all of them:\r");
 		writer.write("#\r");
-		writer.write("# 	¿xpos : Displays your X coordinate;\r");
-		writer.write("# 	¿ypos : Displays your Y coordinate;\r");
-		writer.write("# 	¿zpos : Displays your Z coordinate;\r");
-		writer.write("# 	¿perd : Displays a random number between 1 and 100;\r");
-		writer.write("# 	¿pert : Displays a random number between 1 and 1000;\r");
-		writer.write("# 	¿dice : Displays a random number between 1 and 6;\r");
-		writer.write("# 	¿dim : Displays your dimension;\r");
-		writer.write("# 	¿armor : Displays your armor level;\r");
-		writer.write("# 	¿food : Displays your food level;\r");
-		writer.write("# 	¿hp : Displays your health;\r");
-		writer.write("# 	¿fps : Displays your framerate;\r");
-		writer.write("# 	¿ping : Displays your ping with the server;\r");
-		writer.write("# 	¿name : Displays your username;\r");
-		writer.write("# 	¿lvl : Displays your level;\r");
-		writer.write("# 	¿item : Displays the item you're wielding;\r");
-		writer.write("# 	¿block : Displays the block you are standing on;\r");
-		writer.write("# 	¿players : Displays the amount of players on the server;\r");
-		writer.write("# 	¿prand : Displays the username of a random player on the server;\r");
-		writer.write("# 	¿score : Displays your score (The one that gets shown when you die);\r");
-		writer.write("# 	¿sysdate : Displays the System Date;\r");
-		writer.write("# 	¿systime : Displays the System Time;\r");
-		writer.write("# 	¿timezone : Displays your Timezone;\r");
-		writer.write("# 	¿tex : Displays the name of the Texture Pack you're using;\r");
-		writer.write("# 	¿enchants : Displays the Enchantments on the item you're wielding.\r");
+		writer.write("# 	Â¿xpos : Displays your X coordinate;\r");
+		writer.write("# 	Â¿ypos : Displays your Y coordinate;\r");
+		writer.write("# 	Â¿zpos : Displays your Z coordinate;\r");
+		writer.write("# 	Â¿perd : Displays a random number between 1 and 100;\r");
+		writer.write("# 	Â¿pert : Displays a random number between 1 and 1000;\r");
+		writer.write("# 	Â¿dice : Displays a random number between 1 and 6;\r");
+		writer.write("# 	Â¿dim : Displays your dimension;\r");
+		writer.write("# 	Â¿armor : Displays your armor level;\r");
+		writer.write("# 	Â¿food : Displays your food level;\r");
+		writer.write("# 	Â¿hp : Displays your health;\r");
+		writer.write("# 	Â¿fps : Displays your framerate;\r");
+		writer.write("# 	Â¿ping : Displays your ping with the server;\r");
+		writer.write("# 	Â¿names : Displays the names of all the players on the server;\r");
+		writer.write("# 	Â¿name : Displays your username;\r");
+		writer.write("# 	Â¿lvl : Displays your level;\r");
+		writer.write("# 	Â¿item : Displays the item you're wielding;\r");
+		writer.write("# 	Â¿block : Displays the block you are standing on;\r");
+		writer.write("# 	Â¿players : Displays the amount of players on the server;\r");
+		writer.write("# 	Â¿prand : Displays the username of a random player on the server;\r");
+		writer.write("# 	Â¿score : Displays your score (The one that gets shown when you die);\r");
+		writer.write("# 	Â¿sysdate : Displays the System Date;\r");
+		writer.write("# 	Â¿systime : Displays the System Time;\r");
+		writer.write("# 	Â¿timezone : Displays your Timezone;\r");
+		writer.write("# 	Â¿tex : Displays the name of the Texture Pack you're using;\r");
+		writer.write("# 	Â¿enchants : Displays the Enchantments on the item you're wielding;\r");
+		writer.write("# 	Â¿java : Displays the your version of Java.;\r");
+		writer.write("# 	Â¿os : Displays your Operating System;\r");
+		writer.write("# 	Â¿mods : Displays the amount of mods you have loaded;\r");
+		writer.write("# 	Â¿count : Displays the amount of macros you have;\r");
+		writer.write("# 	Â¿potions : Displays the active potion effects on you and their times.\r");
+		writer.write("#\r");
+		writer.write("# Typing Â¶ in the macro will split the message in various messages at that point, keep that in mind.\r");
+		writer.write("# when making long macros.\r");
 		writer.write("#\r");
 		writer.write("# To use a macro just type in the macro start character, the name of the macro and the macro end character.\r");
 		writer.write("# In the chat menu, you can press ALT to toggle macros, and you can press RCONTROL to toggle the way the chat\r");
 		writer.write("# window reacts to you pressing ENTER, be that closing the GUI or just clearing the message.\r");
 		writer.write("#\r");
-		writer.write("# There's also two prebuilt macros, one named 'macros' that will display the macros you have, and one named\r");
-		writer.write("# 'reload', that will reload your macros so you don't have to close your minecraft everytime you change this file.\r");
+		writer.write("# There's also four prebuilt macros, one named 'macros' that will display the macros you have, one named\r");
+		writer.write("# 'reload', that will reload your macros so you don't have to close your minecraft everytime you change this file,\r");
+		writer.write("# one named 'add' that will open a interface to add macros in-game, and one called 'help', that will give you,\r");
+		writer.write("# information about a macro.\r");
 		writer.write("#\r");
 		writer.write("# Example, with the default settings:\r");
 		writer.write("# Typing in {modpage} would display on the chat as http://bit.ly/s6rL8h.\r");
 		writer.write("#\r");
 		writer.write("# TIP: The TAB character gets nulled in the file parsing, so you can use if for convenience!\r");
 		writer.write("#\r");
+		writer.write("# TIP 2: All lines starting with '#' don't get parsed so you can use them as guidelines.\r");
+		writer.write("#\r");
 		writer.write("# Place your macros here:\r");
-		writer.write("# Codes for Convenience: » ¿\r");
-		writer.write("	\r");
-		writer.write("################# File End.");
+		writer.write("# Codes for Convenience: Â» Â¿ Â¶\r");
+		writer.write("	");
 		writer.close();
 	}
 
 	public static String messWithMessage(String message) {
+		if(message.contains("Â¿")) return parseMacro(message);
+		
 		if(!message.startsWith(Character.toString(macroCharStart)) || !message.endsWith(Character.toString(macroCharEnd)))
 			return message;
 		String newMessage = message;
@@ -166,7 +184,7 @@ public class mod_ChatMacros extends BaseMod implements IUpdateManager, IUMAdvanc
 			String allMacros = "Macro List: ";
 			for(String macro : macrosList)
 				allMacros =	allMacros.concat(macro).concat(", ");
-			allMacros = allMacros.concat("macros, reload.");
+			allMacros = allMacros.concat("macros, reload, add, help(macroname).");
 					
 			ModLoader.getMinecraftInstance().thePlayer.addChatMessage(allMacros);
 			return null;
@@ -175,22 +193,43 @@ public class mod_ChatMacros extends BaseMod implements IUpdateManager, IUMAdvanc
 		if(newMessage.matches("reload")){
 			try {
 				reloadMacros();
-				ModLoader.getMinecraftInstance().thePlayer.addChatMessage("§aMacros Reloaded!");
+				ModLoader.getMinecraftInstance().thePlayer.addChatMessage("Â§aMacros Reloaded!");
 			} catch (IOException e) {
 				e.printStackTrace();
-				ModLoader.getMinecraftInstance().thePlayer.addChatMessage("§cFailed to reload Macros!");
+				ModLoader.getMinecraftInstance().thePlayer.addChatMessage("Â§cFailed to reload Macros!");
 			}
+			return null;
+		}
+		
+		if(newMessage.matches("add")){
+			return "â•©Ã™Ãœ7Yâ€¼54$ÃŠ4ÃCTEâ—‹8â–’â—˜"; //Random stuff I typed in to make sure you can't replicate it, don't mind...
+		}
+		
+		if(newMessage.matches("help")){
+			ModLoader.getMinecraftInstance().thePlayer.addChatMessage("Â§cThat's not how you use the help macro, try help'modpage'.");
+			return null;
+		}
+		
+		if(newMessage.startsWith("help'") && newMessage.endsWith("'")){
+			ModLoader.getMinecraftInstance().thePlayer.addChatMessage(parseHelpMacro(newMessage));
 			return null;
 		}
 		
 		if(macros.containsKey(newMessage)){
 			String macro = macros.get(newMessage);
-			if(macro.contains("¿"))
-			return parseMacro(macros.get(newMessage));
-			return macros.get(newMessage);
+			String parsedMacro;
+			if(macro.contains("Â¿")){
+				parsedMacro = parseMacro(macros.get(newMessage));
+				if(macro.contains("Â¶")) return parsedMacro;
+				return parsedMacro.length() == 0 || parsedMacro.length() > 100 ? null : parsedMacro;
+			}
+			
+			String newMacro = macros.get(newMessage);
+			if(macro.contains("Â¶")) return newMacro;
+			return newMacro.length() == 0 || newMacro.length() >  100 ? null : newMacro;
 		}
 		
-		ModLoader.getMinecraftInstance().thePlayer.addChatMessage("§cThat macro doesn't exist.");
+		ModLoader.getMinecraftInstance().thePlayer.addChatMessage("Â§cThat macro doesn't exist.");
 		return null;
 	}
 	
@@ -216,36 +255,77 @@ public class mod_ChatMacros extends BaseMod implements IUpdateManager, IUMAdvanc
 		closeOnFinish = nbt.getBoolean("closeOnFinish");
 	}
 	
+	public static String parseHelpMacro(String helpMacro){
+		String s = helpMacro;
+		if(helpMacro == "help'help'") return "help'macroname' Â» Tells you what the macro in modname does. Prebuilt.";
+		String macroToCheck = s.replaceAll("help", "").replaceAll("'", "");
+
+		if(macroToCheck.equalsIgnoreCase("add"))
+			return "add Â» Opens the Add Macro GUI so you can add macros in-game. Prebuilt.";
+		if(macroToCheck.equalsIgnoreCase("reload")) 
+			return "reload Â» Reloads the list of macros. Prebuilt.";
+		if(macroToCheck.equalsIgnoreCase("macros")) 
+			return "macros Â» Shows all the loaded macros. Prebuilt.";
+		
+		if(!macrosList.contains(macroToCheck)) return "Â§c That Macro doesn't exist, couldn't check for it.";
+		return macroToCheck + " Â» " + macros.get(macroToCheck);
+	}
+	
+	public static boolean addMacroRemotely(String name, String contents){
+		if(macrosList.contains(name) || name.isEmpty() || contents.isEmpty()) return false;	
+		
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ModLoader.getMinecraftInstance().getAppDir("minecraft"), "Chat Macros.txt"), true));
+			writer.write("\r" + name + "	Â»	" + contents);
+			writer.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static boolean isMacroValid(String name, String contents){
+		return !((name.equalsIgnoreCase("macros") || name.equalsIgnoreCase("reload") || name.equalsIgnoreCase("help") || name.equalsIgnoreCase("add"))) &&  !macrosList.contains(name) && !name.isEmpty() && !contents.isEmpty();
+	}
+	
 	public static String parseMacro(String macro){
 		String s = macro;
 		
 		EntityPlayer p = ModLoader.getMinecraftInstance().thePlayer;
 		Random rand = new Random();
 		
-		s = s.replaceAll("¿xpos", (int)Math.floor(p.posX)+"");
-		s = s.replaceAll("¿ypos", (int)Math.floor(p.posY)+"");
-		s = s.replaceAll("¿zpos", (int)Math.floor(p.posZ)+"");
-		s = s.replaceAll("¿perc", rand.nextInt(100)+"");
-		s = s.replaceAll("¿pert", rand.nextInt(1000)+"");
-		s = s.replaceAll("¿dice", (rand.nextInt(6)+1)+"");
-		s = s.replaceAll("¿dim", p.dimension+"");
-		s = s.replaceAll("¿armor", p.getTotalArmorValue()+"");
-		s = s.replaceAll("¿food", p.foodStats.getFoodLevel()+"");
-		s = s.replaceAll("¿hp", p.health+"");
-		s = s.replaceAll("¿fps", getFps()+"");
-		s = s.replaceAll("¿ping", getPing()+"");
-		s = s.replaceAll("¿name", p.username);
-		s = s.replaceAll("¿lvl", p.experienceLevel+"");
-		s = s.replaceAll("¿item", getHeldItem(p));
-		s = s.replaceAll("¿block", getBlockStanding(p));
-		s = s.replaceAll("¿players", p.worldObj.playerEntities.size()+"");
-		s = s.replaceAll("¿prand", pickRandomPlayer(p.worldObj.playerEntities));
-		s = s.replaceAll("¿score", p.score+"");
-		s = s.replaceAll("¿sysdate", (new SimpleDateFormat("dd/MM/yyyy").format(new Date())));
-		s = s.replaceAll("¿systime", (new SimpleDateFormat("HH:mm").format(new Date())));
-		s = s.replaceAll("¿timezone", Calendar.getInstance().getTimeZone().getDisplayName());
-		s = s.replaceAll("¿tex", (ModLoader.getMinecraftInstance().texturePackList.selectedTexturePack.texturePackFileName.replaceAll(".zip", "")));
-		s = s.replaceAll("¿enchants", getEnchants(p.getCurrentEquippedItem()));
+		s = s.replaceAll("Â¿xpos", (int)Math.floor(p.posX)+"");
+		s = s.replaceAll("Â¿ypos", (int)Math.floor(p.posY)+"");
+		s = s.replaceAll("Â¿zpos", (int)Math.floor(p.posZ)+"");
+		s = s.replaceAll("Â¿perc", rand.nextInt(100)+"");
+		s = s.replaceAll("Â¿pert", rand.nextInt(1000)+"");
+		s = s.replaceAll("Â¿dice", (rand.nextInt(6)+1)+"");
+		s = s.replaceAll("Â¿dim", p.dimension+"");
+		s = s.replaceAll("Â¿armor", p.getTotalArmorValue()+"");
+		s = s.replaceAll("Â¿food", p.foodStats.getFoodLevel()+"");
+		s = s.replaceAll("Â¿hp", p.health+"");
+		s = s.replaceAll("Â¿fps", getFps()+"");
+		s = s.replaceAll("Â¿ping", getPing()+"");
+		s = s.replaceAll("Â¿names", getPlayerNames(p.worldObj));
+		s = s.replaceAll("Â¿name", p.username);
+		s = s.replaceAll("Â¿lvl", p.experienceLevel+"");
+		s = s.replaceAll("Â¿item", getHeldItem(p));
+		s = s.replaceAll("Â¿block", getBlockStanding(p));
+		s = s.replaceAll("Â¿players", p.worldObj.playerEntities.size()+"");
+		s = s.replaceAll("Â¿prand", pickRandomPlayer(p.worldObj.playerEntities));
+		s = s.replaceAll("Â¿score", p.score+"");
+		s = s.replaceAll("Â¿sysdate", (new SimpleDateFormat("dd/MM/yyyy").format(new Date())));
+		s = s.replaceAll("Â¿systime", (new SimpleDateFormat("HH:mm").format(new Date())));
+		s = s.replaceAll("Â¿timezone", Calendar.getInstance().getTimeZone().getDisplayName());
+		s = s.replaceAll("Â¿tex", (ModLoader.getMinecraftInstance().texturePackList.selectedTexturePack.texturePackFileName.replaceAll(".zip", "")));
+		s = s.replaceAll("Â¿enchants", getEnchants(p.getCurrentEquippedItem()));
+		s = s.replaceAll("Â¿java", System.getProperty("java.version"));
+		s = s.replaceAll("Â¿os", System.getProperty("os.name"));
+		s = s.replaceAll("Â¿mods", ModLoader.getLoadedMods().size()+"");
+		s = s.replaceAll("Â¿count", macrosList.size()+"");
+		s = s.replaceAll("Â¿potions", getPotions(p));
+		
 		return s;
 	}
 	
@@ -314,10 +394,42 @@ public class mod_ChatMacros extends BaseMod implements IUpdateManager, IUMAdvanc
 				enchs = enchs.concat(Enchantment.enchantmentsList[cp.getShort("id")].getTranslatedName(cp.getShort("lvl")) + ", ");
 			}
 			if(enchs.endsWith(", "))
-				enchs = enchs.substring(0, enchs.length()-2);
-			
+				return enchs.substring(0, enchs.length()-2);
 			return enchs;
 		}else return "null";
+	}
+	
+	static String getPlayerNames(World world){
+		List<EntityPlayer> l = world.playerEntities;
+		String playerList = "";
+		
+		for(EntityPlayer e : l)
+			playerList = playerList.concat(e.username + ", ");
+		if(playerList.endsWith(", "))
+			return playerList.substring(0, playerList.length()-2);
+		return playerList;
+	}
+	
+	static String getPotions(EntityLiving e){
+		Collection<PotionEffect> l = e.getActivePotionEffects();
+		String effects = "";
+		
+		if(l.size() == 0) return "null";
+		
+		for(PotionEffect p : l)
+			effects = effects.concat(StatCollector.translateToLocal(p.getEffectName()) + " " + getPotionDuration(p) + ", ");
+				
+		if(effects.endsWith(", "))
+			return effects.substring(0, effects.length()-2);
+		return effects;
+	}
+	
+	static String getPotionDuration(PotionEffect effect){
+        int var1 = effect.getDuration();
+        int var2 = var1 / 20;
+        int var3 = var2 / 60;
+        var2 %= 60;
+        return var2 < 10 ? var3 + ":0" + var2 : var3 + ":" + var2;
 	}
 	
 	public static void reloadMacros() throws IOException{
